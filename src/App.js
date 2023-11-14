3302f9895c9bd65ea0945a15c44b7e0df31d4495
pouriaafshari <pooriyaafshari81@gmail.com>
2023-11-14 20:29:43 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Loading from './Loading';
-import Editor from './Editor';
-import Login from './Login';
+
 import './App.css';
 
-// Create an AuthContext
-const AuthContext = createContext();
-
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!loggedIn) {
-      return <Navigate to="/React-photo-editor/" />;
-    }
-
-    return children;
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
-
+export default function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <Routes>
-          <Route path="/React-photo-editor/" element={<Login />} />
-          <Route path="/React-photo-editor/editor" element={<Editor />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/editor" element={<Editor />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+function Login() {
+  return(<div>Login</div>)
+}
+function Editor() {
+  return(<div>Editor</div>)
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App'
-import Login from './Login';
-import Editor from './Editor';
-import Loading from './Loading';
 
 ReactDOM.render(
   <React.StrictMode>
