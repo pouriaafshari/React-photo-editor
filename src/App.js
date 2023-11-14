5c64d676b53bb1f5011535896f797200a526c610
pouriaafshari <pooriyaafshari81@gmail.com>
2023-11-14 18:22:03 +0100

    reboot

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React, { useState, useEffect, useContext, createContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Loading from './Loading';
-import Editor from './Editor';
-import Login from './Login';
+import logo from './logo.svg';
 import './App.css';
 
-// Create an AuthContext
-const AuthContext = createContext();
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!loggedIn) {
-      return <Navigate to="/React-photo-editor/" />;
-    }
-
-    return children;
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
-    <div className="container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/React-photo-editor/" element={<Login />} />
-          <Route path="/React-photo-editor/editor" element={<Editor />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
