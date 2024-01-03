e95771a5f457addfdefdd71a0a019ecd7cbbf802
pouriaafshari <pooriyaafshari81@gmail.com>
2024-01-03 13:01:39 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 import Login from './Login'
 import Editor from './Editor'
 
 export default function App() {
+
+  const [currentUser, setCurrentUser] = useState(false);
+
+  const LoggedIn = () => {
+    alert("ok");
+    setCurrentUser(true);
+  }
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
+    }
+  
+    return children;
+  };
+
   return (
     <div className="container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/editor" element={<Editor />} />
-        </Routes>
+          <Route path="/" element={<Login LoggedIn={LoggedIn}/>} />
+          <Route
+            path="/editor"
+            element={
+              <ProtectedRoute>
+                <Editor />
+              </ProtectedRoute>
+            }
+          />
+          </Routes>
       </BrowserRouter>
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,105 @@
-// Login.jsx
-
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import "./Login.css"; // Import your CSS file
 import { Link, Navigate } from "react-router-dom";
+const LoginUrl = 'https://photo-editor-d8if.onrender.com/login';
+const SignupUrl = 'https://photo-editor-d8if.onrender.com/signup';
 
-export default function Login() {
+export default function Login({LoggedIn}) {
   const [isSignUp, setSignUp] = useState(false);
-  const Loggedin = createContext(false)
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isFormValid, setFormValid] = useState(false);
 
   const handleSwitchToggle = () => {
     setSignUp(!isSignUp);
   };
 
-  function handleLogin() {
-    
+  useEffect(() => {
+    // Implement form validation rules here
+    const isUsernameValid = /^[a-zA-Z]+$/u.test(username);
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isPasswordValid = password.length >= 8;
+    const isConfirmPasswordValid = isSignUp ? password === confirmPassword : true;
+
+    // Enable/disable the submit button based on validation
+    if (isSignUp){
+      setFormValid(isUsernameValid && isEmailValid && isPasswordValid && isConfirmPasswordValid);
+    }
+    else {
+      setFormValid(isUsernameValid && isPasswordValid);
+    }
+  }, [isSignUp, username, email, password, confirmPassword]);
+
+  const handleLogin = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (isSignUp) {
+      // Call signup function
+      const email = document.getElementById('email').value;
+      handleSignUp(username, password, email);
+    } else {
+      // Call login function
+      LoginCheck(username, password);
+    }
+  };
+
+  async function LoginCheck(username, password) {
+    try {
+      const response = await fetch(LoginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('Login successful:', data);
+      
+
+    } catch (error) {
+      console.error('Error during login:', error.message);
+      // Handle errors as needed
+    }
+  }
+
+  async function handleSignUp(username, password, email) {
+    try {
+      const response = await fetch(SignupUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          email: email,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log('Signup successful:', data);
+      if (data === 'yes') {
+        LoggedIn();
+      }
+
+    } catch (error) {
+      console.error('Error during signup:', error.message);
+    }
   }
 
   return (
@@ -28,23 +114,62 @@ export default function Login() {
       <div className="input-container">
         {isSignUp ? 
         <>
-            <input placeholder="Username" className="login-input"/>
+            <input
+              id="username"
+              placeholder="Username"
+              className="login-input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
             <br />
-            <input placeholder="Email" className="login-input"/>
+            <input
+              id="email"
+              placeholder="Email"
+              className="login-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <br />
-            <input type="password" placeholder="Password" className="login-input"/>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              className="login-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <br />
-            <input type="password" placeholder="Confirm Password" className="login-input"/>
+            <input
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              className="login-input"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
             <div className="password-helper">*Password must be at least 8 characters long*</div>
         </> :
         <>
-            <input placeholder="Username" className="login-input"/>
+            <input
+              id="username"
+              placeholder="Username"
+              className="login-input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
             <br />
-            <input type="password" placeholder="Password" className="login-input"/>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              className="login-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
         </>}
         <br />
-        <button className={isSignUp?"signup-button":"login-button"} onClick={handleLogin}>
-          {isSignUp?"Sign Up":"Login"}
+        <button className={isSignUp ? "signup-button" : "login-button"} onClick={handleLogin} disabled={!isFormValid}>
+          {isSignUp ? "Sign Up" : "Login"}
         </button>
         <br />
         <button className="google-login">Login with Google</button>
